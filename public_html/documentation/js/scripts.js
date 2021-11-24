762d188a9b59a75b2a737820509575d8d266b0a8
Nels Bjork <nbjork1@cnm.edu>
2021-11-23 19:27:35 -0600

    contact form

--- public_html/documentation/js/scripts.js
+++ public_html/documentation/js/scripts.js
@@ -1,6 +1,6 @@
 
 //
-// Scripts
+// Scrip?ts
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
@@ -48,3 +48,54 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Frontend Validation
+$(document).ready(function () {
+    $("#contact").validate({
+        debug:true,
+        errorClass: "alert alert-danger",
+        errorLabelContainer: "#output-area",
+        errorElement: "div",
+        // rules here define what is good or bad input
+        //each rule starts with the form input element's NAME attribute
+        rules:{
+            name: {
+                required: true
+            } ,
+            email: {
+                email: true,
+                required: true
+            },
+            message: {
+                required: true,
+                maxlength: 2000,
+            }
+        },
+        messages:{
+            name: {
+                required: "Name is a required field"
+            },
+            email: {
+                email: "Please provide a valid email address",
+                required: "Email is a required field"
+            },
+            message: {
+                required: "Message is a required field",
+                maxlength: "Message is to long"
+            }
+        },
+        submitHandler: (form) => {
+            $("#contact").ajaxSubmit({
+                type: "POST",
+                url: $("#contact").attr('action'),
+                success: (ajaxOutput) => {
+                    $("#output-area").css("display", "")
+                    $("#output-area").html(ajaxOutput)
+                    if($(".alert-success" >= 1)) {
+                        $("#contact")[0].reset()
+                    }
+                }
+            })
+        }
+    })
+})
